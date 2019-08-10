ceaac08b612b970a0e3eea2000b08e7f5e670e6b
nandhakumarv5858 <46603021+nandhakumarv5858@users.noreply.github.com>
2019-08-10 21:14:32 +0530

    Create class using print.py

---
+++ class using print.py
@@ -0,0 +1,10 @@
+class tring:
+    def __init__(self):
+        self.str2=" "
+    def get_string(self):
+        self.str2=input()
+    def print_string(self):
+        print(self.str2.upper())
+c = tring()
+c.get_string()
+c.print_string()
